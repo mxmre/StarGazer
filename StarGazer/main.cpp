3a8d31c493ebf308cff5e6101b5d917988656544
mxmre <mxmkalinkin@gmail.com>
2023-05-05 23:47:00 +0300

    проверка

--- StarGazer/main.cpp
+++ StarGazer/main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 using namespace sg::utility;
 int main()
 {
-	while(true)
-	std::cout << DateTime::ToString(DateTime::Now()) << std::endl;
+	while (true)
+	{
+		std::cout << DateTime::ToString(DateTime::Now()) << std::endl;
+	}
+	
 	return 0;
 }
